2c91a63186e69880f4258b94c071d6bb6ee794e4
Karlen Speiser <karlen@Karlens-MacBook-Air.local>
2022-09-16 11:44:43 -0700

    Feature1:  Sorting the array
    This feature will sort an array of random numbers.

---
+++ base.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class base {
+	public static void main(String [] args) {
+		int [] arr1 = new int[10];
+		for(int i=0;i<arr1.length;i++) {
+			int rand = ((int) (Math.random()*100));
+			arr1[i] = rand;
+		}
+		System.out.println("Here's an array: ");
+		for(int j=0;j<arr1.length;j++) {
+			System.out.print(arr1[j]+" ");
+		}
+		System.out.println("\n");
+		System.out.println("Here is the array sorted:");
+		Arrays.sort(arr1);
+		
+		for(int j=0;j<arr1.length;j++) {
+			System.out.print(arr1[j]+" ");
+		}
+	}
+}
