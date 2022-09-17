2ecf502f46858748cda63a60c935dc9961138d4f
Karlen Speiser <karlen@Karlens-MacBook-Air.local>
2022-09-16 18:28:20 -0700

    Add average method for feature 2

--- base2.java
+++ base2.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 
-public class base {
+public class base2 {
 	public static void main(String [] args) {
 		int [] arr1 = new int[10];
 		int sum = 0;
-		int above = 0;
 		for(int i=0;i<arr1.length;i++) {
+			
 			int rand = ((int) (Math.random()*100));
 			arr1[i] = rand;
 			sum = sum + rand;
@@ -14,11 +14,15 @@ public class base {
 		for(int j=0;j<arr1.length;j++) {
 			System.out.print(arr1[j]+" ");
 		}
-		int avg = sum/10;
+		average(arr1,sum);
+	}
+	static void average(int[] array, int sum1) {
+		int above = 0;
+		int avg = sum1/10;
 		System.out.println("\n");
 		System.out.println("The average of the array is: ("+avg+")");
-		for(int k = 0;k<arr1.length;k++) {
-			if(arr1[k]>avg) {
+		for(int k = 0;k<array.length;k++) {
+			if(array[k]>avg) {
 				above++;
 			}
 		}
